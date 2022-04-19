5d4a668edf9a1245596081d42419197b140a0f60
adam.harrison <adam.h.dev1@gmail.com>
2022-04-19 15:38:18 +0100

    service tests added for getQuoteById

--- src/test/kotlin/uk/co/binxly/services/QuotesServiceImplTest.kt
+++ src/test/kotlin/uk/co/binxly/services/QuotesServiceImplTest.kt
@@ -14,6 +14,8 @@ import uk.co.binxly.repository.KanyeQuotes
 import uk.co.binxly.utils.Fixtures.KANYE
 import uk.co.binxly.utils.Fixtures.testQuote
 
+const val QUOTE_ID = "015"
+const val INVALID_QUOTE_ID = "106"
 val quotesServiceImpl = QuotesServiceImpl()
 
 @QuarkusTest
@@ -26,23 +28,44 @@ internal class QuotesServiceImplTest {
     }
 
     @Test
-    fun service_calls_kanye_repository() {
+    fun get_quote_calls_kanye_repository() {
         every { KanyeQuotes.getQuote() } returns testQuote
         quotesServiceImpl.getQuote(KANYE)
         verify(exactly = 1) { KanyeQuotes.getQuote() }
     }
 
     @Test
-    fun returns_quote_from_kanye_repository() {
+    fun get_quote_returns_quote_from_kanye_repository() {
         every { KanyeQuotes.getQuote() } returns testQuote
         val result = quotesServiceImpl.getQuote(KANYE)
         assertEquals(testQuote, result)
     }
 
     @Test
-    fun returns_null_if_category_not_recognised() {
+    fun get_quote_returns_null_if_category_not_recognised() {
         every { KanyeQuotes.getQuote() } returns null
         val result = quotesServiceImpl.getQuote("notCategory")
+        assertNull(result)
+    }
+
+    @Test
+    fun get_quote_by_id_calls_kanye_repository() {
+        every { KanyeQuotes.getQuoteById(QUOTE_ID) } returns testQuote
+        quotesServiceImpl.getQuoteById(KANYE, QUOTE_ID)
+        verify(exactly = 1) { KanyeQuotes.getQuoteById(QUOTE_ID) }
+    }
+
+    @Test
+    fun get_quote_by_id_returns_quote_from_kanye_repository() {
+        every { KanyeQuotes.getQuoteById(QUOTE_ID) } returns testQuote
+        val result = quotesServiceImpl.getQuoteById(KANYE, QUOTE_ID)
+        assertEquals(testQuote, result)
+    }
+
+    @Test
+    fun get_quote_by_id_returns_null_if_category_not_recognised() {
+        every { KanyeQuotes.getQuoteById(INVALID_QUOTE_ID) } returns null
+        val result = quotesServiceImpl.getQuoteById(KANYE, INVALID_QUOTE_ID)
         assertNull(result)
     }
 
